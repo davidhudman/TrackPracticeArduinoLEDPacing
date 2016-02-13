5707eb10479773e86423258d6b9272e77f84fb07
David Hudman <davidhudman@hotmail.com>
2016-02-12 19:41:07 -0600

    no real change to HTML, just needed to add selectPin.php to the repository

---
+++ selectPin.php
@@ -0,0 +1,18 @@
+<?php
+// updateDBandYun.php
+
+$db = new SQLite3('/mnt/sda1/arduino/www/TrackPractice/pacer.db');
+
+$echoString = "";
+
+$results = $db->query('SELECT * FROM Pin');
+
+while ($row = $results->fetchArray()) {
+			$echoString = $echoString . $row['pacerIndex'] . " | " . $row['passcode'] . " | " . $row['active'] . " | " . $row['color'] . " | " . $row['lapTime'] . " | " . $row['name'] . "<br />";
+}
+$db->exec($query);
+$db->close();
+
+echo $echoString;
+
+?>

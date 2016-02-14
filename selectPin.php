961360239c32ccc9b272979ac6d8594653e2551f
David Hudman <davidhudman@hotmail.com>
2016-02-14 12:37:50 -0600

    Added capability to ask for "Select * From Pin" to return with "\n" instead of "<br />" for line breaks

--- selectPin.php
+++ selectPin.php
@@ -4,11 +4,28 @@
 $db = new SQLite3('/mnt/sda1/arduino/www/TrackPractice/pacer.db');
 
 $echoString = "";
+$backslashNNeeded = -1;
+
+// Pull all your values out of the GET request
+if (empty($_REQUEST["backslashNNeeded"])) {
+	$backslashNNeeded = $_REQUEST["backslashNNeeded"];
+}
+else {
+	// do nothing - for some reason it always gets recognized as "empty" even though there are clearly parameters
+	$backslashNNeeded = $_REQUEST["backslashNNeeded"];
+}
 
 $results = $db->query('SELECT * FROM Pin');
 
-while ($row = $results->fetchArray()) {
-			$echoString = $echoString . $row['pacerIndex'] . " | " . $row['passcode'] . " | " . $row['active'] . " | " . $row['color'] . " | " . $row['lapTime'] . " | " . $row['name'] . "<br />";
+if ($backslashNNeeded == 1) { 
+	while ($row = $results->fetchArray()) {
+				$echoString = $echoString . $row['pacerIndex'] . " | " . $row['passcode'] . " | " . $row['active'] . " | " . $row['color'] . " | " . $row['lapTime'] . " | " . $row['name'] . "\n";
+	}
+}
+else {
+	while ($row = $results->fetchArray()) {
+				$echoString = $echoString . $row['pacerIndex'] . " | " . $row['passcode'] . " | " . $row['active'] . " | " . $row['color'] . " | " . $row['lapTime'] . " | " . $row['name'] . "<br />";
+	}
 }
 $db->exec($query);
 $db->close();
